6f2c46eedd68530171cc87648854f08dcafd553f
Tim Meusel <tim@bastelfreak.de>
2024-05-21 15:51:39 +0200

    nil

--- lib/beaker/hypervisor/vcloud.rb
+++ lib/beaker/hypervisor/vcloud.rb
@@ -51,6 +51,7 @@ module Beaker
 
     def booting_host(host, try, attempts)
       @logger.notify "Booting #{host['vmhostname']} (#{host.name}) and waiting for it to register with vSphere"
+      sleep 10
       until @vsphere_helper.find_vms(host['vmhostname'])[host['vmhostname']].summary.guest.toolsRunningStatus == 'guestToolsRunning' and
             !@vsphere_helper.find_vms(host['vmhostname'])[host['vmhostname']].summary.guest.ipAddress.nil?
         raise "vSphere registration failed after #{@options[:timeout].to_i} seconds" unless try <= attempts
